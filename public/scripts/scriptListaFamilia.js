1bd6e53a09172abcf10d14a86d139fe91829d2f4
EPerezaf <eperez.af@gmail.com>
2025-11-05 16:21:49 -0600

    Empezando con lista de Familias

---
+++ public/scripts/scriptListaFamilia.js
@@ -0,0 +1,7 @@
+//FUNCION PARA MOSTRAR LAS FAMILIAS GUARDADAS
+//LISTAFAMILIA.HTML
+
+async function cargarFamilias() {
+    const contenedor = document.getElementById("contenedorFamilias");
+    contenedor.innerHTML
+}
